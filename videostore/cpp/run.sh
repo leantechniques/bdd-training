7207bfc46d6d385c318bfe49cdce93af926c9e16
Tim Gifford <tgifford@gmail.com>
2015-08-27 10:06:35 -0500

    pass generator to cmake when supplied to run

--- videostore/cpp/run.sh
+++ videostore/cpp/run.sh
@@ -1,6 +1,14 @@
 gmock=`cd ../../lib/gmock-1.7.0/; pwd`
 export GMOCK_HOME=$gmock
-cmake .
+
+if [ -z $1 ]
+  then
+    echo "No arguments supplied"
+    cmake .
+  else
+    echo "Build using generator" + $1
+    cmake . -G "$1"
+fi
 make all
 
 ./testApp
