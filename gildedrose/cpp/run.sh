f9bf39f6b73598ef30fb357ca53ffe4842126d2b
Tim Gifford <tgifford@gmail.com>
2016-04-14 22:05:06 -0500

    update cpp build file
    run.sh now builds gtest before the project.

--- gildedrose/cpp/run.sh
+++ gildedrose/cpp/run.sh
@@ -1,6 +1,15 @@
 gmock=`cd ../../lib/gmock-1.7.0/; pwd`
 export GMOCK_HOME=$gmock
+
+# Make gmock and gmock test
+cd $GMOCK_HOME
 cmake .
 make all
 
+# Make project
+cd -
+cmake .
+make all
+
+# Run the tests
 ./testApp
