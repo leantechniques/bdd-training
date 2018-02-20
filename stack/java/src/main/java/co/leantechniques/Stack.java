02f697fd2417cce3de4fc5b0e017ea9e6d71a8a1
Tim Gifford <tgifford@gmail.com>
2018-02-20 09:35:46 -0600

    Implement stack with linkedlist

--- stack/java/src/main/java/co/leantechniques/Stack.java
+++ stack/java/src/main/java/co/leantechniques/Stack.java
@@ -1,5 +1,23 @@
 package co.leantechniques;
 
-public class Stack {
+import java.util.LinkedList;
 
+public class Stack<T> {
+
+    private LinkedList<T> _innerList = new LinkedList<>();
+
+    public int size() {
+        return _innerList.size();
+    }
+
+    public Stack put(T value) {
+        _innerList.add(value);
+        return this;
+    }
+
+    public T pop() {
+        if(_innerList.isEmpty()) throw new IndexOutOfBoundsException();
+
+        return _innerList.removeLast();
+    }
 }
--- stack/java/src/test/java/co/leantechniques/StackTest.java
+++ stack/java/src/test/java/co/leantechniques/StackTest.java
@@ -2,13 +2,50 @@ package co.leantechniques;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 public class StackTest {
-  @Test
-  public void isNotNull() {
-    assertThat(new Stack(), is(notNullValue()));
-  }
+    @Test
+    public void isNotNull() {
+        assertThat(new Stack(), is(notNullValue()));
+    }
+
+    @Test
+    public void emptyListHasSizeOfZero() {
+        assertThat(new Stack().size(), is(0));
+    }
+
+    @Test
+    public void putItemIncreasesSize() {
+        Stack stack = new Stack();
+        stack.put("1");
+        stack.put("2");
+
+        assertThat(stack.size(), is(2));
+    }
+
+    @Test
+    public void popReturnsLastItem() {
+        Stack<String> stack = new Stack<>();
+        stack.put("1");
+        stack.put("2");
+
+        assertThat(stack.pop(), is("2"));
+    }
+
+    @Test()
+    public void popEmptyStackThrowsException(){
+        Stack<String> stack = new Stack<>();
+
+        try {
+            stack.pop();
+            fail("expected exception");
+        } catch (Exception e) {
+            assertThat(e, instanceOf(IndexOutOfBoundsException.class));
+        }
+    }
 }
