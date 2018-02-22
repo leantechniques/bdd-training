08f753becec85c928438feebf45df02a47c45cc4
Tim Gifford <tgifford@gmail.com>
2018-02-21 23:50:43 -0600

    Finished Stack for csharp

--- stack/csharp/src/Stack.Tests/StackTest.cs
+++ stack/csharp/src/Stack.Tests/StackTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using stack;
 
 namespace StackTest
@@ -6,13 +7,38 @@ namespace StackTest
     [TestFixture]
     public class StackTest
     {
-        [Test]
-        public void Truthyness()
-        {
-            var stack = new Stack();
+        private Stack<string> _stack;
 
-            Assert.IsNotNull(stack);
-            Assert.AreEqual(true, true);
-        } 
+        [SetUp]
+        public void Setup()
+        {
+            _stack = new Stack<string>();
+        }
+
+        [Test]
+        public void NewStackIsEmpty()
+        {
+            Assert.AreEqual(0, _stack.Count());
+        }
+
+        [Test]
+        public void PutIncreasesCountByOne()
+        {
+            Assert.AreEqual(1, _stack.Put("a").Count());
+        }
+
+        [Test]
+        public void PopReturnsLastItemAdded()
+        {
+            Assert.AreEqual("z", _stack.Put("a").Put("z").Pop());
+            Assert.AreEqual("a", _stack.Pop());
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void PopEmptyStackThrowsException()
+        {
+            _stack.Pop();
+        }
     }
 }
--- stack/csharp/src/Stack/Stack.cs
+++ stack/csharp/src/Stack/Stack.cs
@@ -1,7 +1,33 @@
-﻿namespace stack
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace stack
 {
-    public class Stack
+    public class Stack<T>
     {
-         
+        private readonly List<T> _data = new List<T>();
+
+        public int Count()
+        {
+            return _data.Count;
+        }
+
+        public Stack<T> Put(T s)
+        {
+            _data.Add(s);
+            return this;
+        }
+
+        public T Pop()
+        {
+            var poppedValue = _data[GetLastIndex()];
+            _data.RemoveAt(GetLastIndex());
+            return poppedValue;
+        }
+
+        private int GetLastIndex()
+        {
+            return Count() - 1;
+        }
     }
 }
