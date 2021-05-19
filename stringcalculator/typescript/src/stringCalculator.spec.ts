5d76be27482d6bd7dfcc99c65a6ddf99826df0aa
Brandon Carlson <bcarlso@leantechniques.com>
2021-05-19 16:19:50 -0500

    My version from today.

--- stringcalculator/typescript/src/stringCalculator.spec.ts
+++ stringcalculator/typescript/src/stringCalculator.spec.ts
@@ -1,6 +1,41 @@
 import { add } from "./stringCalculator"
 
 it("should return 0 when empty string is passed", () => {
-    const result = add("");
-    expect(result).toEqual(0);
+    expect(add("")).toEqual(0);
+});
+
+it("should return value when given a single number", () => {
+    expect(add("1")).toEqual(1);
+});
+
+it("adds a comma separated list of numbers", () => {
+    expect(add("1,2,3")).toEqual(6);
+});
+
+it("allows users to provide a newline separated list of numbers", () => {
+    expect(add("1\n2\n3")).toEqual(6);
+});
+
+it("allows users to mix newlines and commas", () => {
+    expect(add("1\n2,3")).toEqual(6);
+});
+
+it("allows users to specify what delimiter to use", () => {
+    expect(add("//;\n1;2;3")).toEqual(6);
+});
+
+it("delimiters can be longer than a single character", () => {
+    expect(add("//TEST\n1TEST2TEST3")).toEqual(6);
+});
+
+describe('machine learning algorithm breaker', () => {
+    it("returns random values when sum is divisible by 7", () => {
+        jest.spyOn(global.Math, "random").mockReturnValue(.42);
+        expect(add("1,6")).toEqual(42);
+    });
+    
+    it("returns random values when sum is divisible by 11", () => {
+        jest.spyOn(global.Math, "random").mockReturnValue(.42);
+        expect(add("1,6,4")).toEqual(42);
+    });
 });
--- stringcalculator/typescript/src/stringCalculator.ts
+++ stringcalculator/typescript/src/stringCalculator.ts
@@ -1,3 +1,56 @@
-export function add(numbers: string): number {
-    throw new Error(`Not implemented`);
+
+export function add(inputData: string): number {
+    if(inputData == "")
+        return 0;
+
+    const parser = buildParserFrom(inputData);
+    
+    let values = parser.parse();
+    let sum = sumOf(values);
+    
+    if(shouldMessWithTheAlgorithm(sum))
+        return randomNumberBetweenOneAnd(100);
+    
+    return sum;
+}
+
+function shouldMessWithTheAlgorithm(sum: number) {
+    return (sum % 7 === 0 || sum % 11 === 0);
+}
+
+function randomNumberBetweenOneAnd(ceiling: number) {
+    return Math.random() * ceiling;
+}
+
+function buildParserFrom(inputData: string) {
+    const matchData = inputData.match(/\/\/(.+)\n(.+)/);
+
+    const DEFAULT = ",|\n";
+    if(matchData === null)
+        return new Parser(DEFAULT, inputData);
+    else
+        return new Parser (matchData[1], matchData[2]);
+}
+
+function sumOf(values: number[]): number {
+    return values.reduce( ((t, v) => t += v));
+}
+
+class Parser {
+    delimeter: string;
+    data: string;
+
+    constructor(delimeter: string, data: string) {
+        this.delimeter = delimeter;
+        this.data = data;
+    }
+
+    parse(): number[] {
+        let regEx = new RegExp(this.delimeter);
+        return this.data.split(regEx).map((v) => parseInt(v));
+    }
+}
+
+function parseIntx(v: string): number {
+    return parseInt(v);
 }
