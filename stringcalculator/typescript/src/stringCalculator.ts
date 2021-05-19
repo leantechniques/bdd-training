12bf1a8a6241417627194a4dddd7218cb28aa542
Jess Alexander <alexanderjessmarie@gmail.com>
2021-05-19 11:46:28 -0500

    small team solution

--- stringcalculator/typescript/src/stringCalculator.ts
+++ stringcalculator/typescript/src/stringCalculator.ts
@@ -1,10 +1,18 @@
 export function add(numbers: string): number {
-    
-    if(numbers == '')
-        return 0;
-    const result = numbers.split(/,|\n/)
-    
-    let sum = result.reduce((acc, r)=> acc+parseInt(r),0);
-    
-    return sum;
-}
+    if (numbers.length === 0) {
+      return 0;
+    }
+    const regex = new RegExp("^(//)(?<delimiter>.+)(\\n)(?<numbers>.+)").exec(numbers);
+    const delim = regex?.groups?.["delimiter"];
+  
+    let numbersArray: string[] | undefined;
+    numbersArray = numbers.split(/,|\n/);
+  
+    if (delim) {
+      const numberString: string | undefined = regex?.groups?.["numbers"];
+      numbersArray = numberString?.split(delim);
+    }
+  
+    // @ts-ignore
+    return numbersArray.reduce((sum, val) => sum + parseInt(val), 0);
+  }
