ac5c49913e485b947e2743a06e3cfb1e41f6e220
Jess Alexander <alexanderjessmarie@gmail.com>
2021-05-19 11:03:00 -0500

    optimize solution

--- stringcalculator/typescript/src/stringCalculator.ts
+++ stringcalculator/typescript/src/stringCalculator.ts
@@ -3,7 +3,8 @@ export function add(numbers: string): number {
     if(numbers == '')
         return 0;
     const result = numbers.split(',')
-    let sum = 0;
-    result.forEach(r => sum += parseInt(r));
+    
+    let sum = result.reduce((acc, r)=> acc+parseInt(r),0);
+    
     return sum;
 }
